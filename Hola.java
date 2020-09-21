ff264a17c34bca8780fa7fdae61c5f10176a06ad
BlueScreenCQ <38056342+BlueScreenCQ@users.noreply.github.com>
2020-09-21 18:57:46 +0200

    Update Hola.java

--- Hola.java
+++ Hola.java
@@ -1,6 +1,6 @@
 class main{
 
   public static void main(){
-    system.out.println("Principal Branch");
+    system.out.println("Second Branch. Changes here don't affect the main branch");
   }
  }
